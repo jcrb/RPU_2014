310dcd15610473abc7c4422b234e697410925bac
JCB <jeanclaude.bartier@gmail.com>
2014-08-04 22:08:57 +0200

    maj 1er aout

--- Preparation/RPU Quotidiens/quot_utils.R
+++ Preparation/RPU Quotidiens/quot_utils.R
@@ -52,7 +52,7 @@ finess2hop <- function(a){
   a[a=="670000397"]<-"Sel"
   a[a=="680000684"]<-"Col"
   a[a=="670016237"]<-"Odi"
-  a[a=="670000272"]<-"Wis"
+  a[a=="670016237"]<-"Wis"
   a[a=="680000700"]<-"Geb"
   a[a=="670780055"]<-"Hus"
   a[a=="680000197"]<-"3Fr"
@@ -284,4 +284,58 @@ rpu2xts <- function(d){
   a$total <- rowSums(a[,1:14])
   ts <- xts(a, order.by = a$date)
   ts
+}
+
+
+#=======================================
+#
+# plot.xts2
+#
+#=======================================
+# La méthode plot.xts comprte un bug qui empêche l'affichage de courbes en couleur. Cette version corrige le bug.
+#'@author Roman Luštrik (http://stackoverflow.com/users/322912/roman-lu%c5%a1trik)
+#'@source http://stackoverflow.com/questions/9017070/set-the-color-in-plot-xts
+#'
+plot.xts2 <- function (x, y = NULL, type = "l", auto.grid = TRUE, major.ticks = "auto", 
+                       minor.ticks = TRUE, major.format = TRUE, bar.col = "grey", 
+                       candle.col = "white", ann = TRUE, axes = TRUE, col = "black", ...) 
+{
+  series.title <- deparse(substitute(x))
+  ep <- axTicksByTime(x, major.ticks, format = major.format)
+  otype <- type
+  if (xts:::is.OHLC(x) && type %in% c("candles", "bars")) {
+    x <- x[, xts:::has.OHLC(x, TRUE)]
+    xycoords <- list(x = .index(x), y = seq(min(x), max(x), 
+                                            length.out = NROW(x)))
+    type <- "n"
+  }
+  else {
+    if (NCOL(x) > 1) 
+      warning("only the univariate series will be plotted")
+    if (is.null(y)) 
+      xycoords <- xy.coords(.index(x), x[, 1])
+  }
+  plot(xycoords$x, xycoords$y, type = type, axes = FALSE, ann = FALSE, 
+       col = col, ...)
+  if (auto.grid) {
+    abline(v = xycoords$x[ep], col = "grey", lty = 4)
+    grid(NA, NULL)
+  }
+  if (xts:::is.OHLC(x) && otype == "candles") 
+    plot.ohlc.candles(x, bar.col = bar.col, candle.col = candle.col, 
+                      ...)
+  dots <- list(...)
+  if (axes) {
+    if (minor.ticks) 
+      axis(1, at = xycoords$x, labels = FALSE, col = "#BBBBBB", 
+           ...)
+    axis(1, at = xycoords$x[ep], labels = names(ep), las = 1, 
+         lwd = 1, mgp = c(3, 2, 0), ...)
+    axis(2, ...)
+  }
+  box()
+  if (!"main" %in% names(dots)) 
+    title(main = series.title)
+  do.call("title", list(...))
+  assign(".plot.xts", recordPlot(), .GlobalEnv)
 }
