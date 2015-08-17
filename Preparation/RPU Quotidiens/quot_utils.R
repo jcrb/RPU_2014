e9b1bf86cd8be44871e4b4f57042673fa6c6936f
JCB <jeanclaude.bartier@gmail.com>
2015-08-17 22:50:35 +0200

    15 aout 2015

--- Preparation/RPU Quotidiens/quot_utils.R
+++ Preparation/RPU Quotidiens/quot_utils.R
@@ -76,7 +76,7 @@ finess2hop <- function(a){
   a[a=="670783273"]<-"HTP" # HTP maj le 17/10/2014
   a[a=="680000197"]<-"3Fr"
   a[a=="680020096"]<-"3Fr" # maj le 30/5/2014 680020096
-  a[a=="680000627"]<-"Mul" # correspond au Hasenrain
+  a[a=="680000627"]<-"Mul" # correspond au Hasenrain 14/08/2015
   a[a=="670000157"]<-"Hag"
   a[a=="680000320"]<-"Dia" # DFO
   a[a=="680000395"]<-"Alk"
